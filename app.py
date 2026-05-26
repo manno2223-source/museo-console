30df78d572f4894c457946649c6c89cf7dfbf895
chri11 <manno2223@giorgimi.edu.it>
2026-05-26 16:44:58 +0200

    Fix finale per Vercel WSGI

--- app.py
+++ app.py
@@ -59,3 +59,8 @@ def aggiungi():
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000, use_reloader=False)
+    # Questa riga serve a Vercel per trovare l'applicazione!
+app = app
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000, use_reloader=False)
